2e82088acf91bac95c8bd60667004e3353002257
Hassan Khaled <58123330+HassanKha@users.noreply.github.com>
2025-05-23 21:42:59 +0300

    Initial commit

--- src/Modules/Shared/MasterLayout.jsx
+++ src/Modules/Shared/MasterLayout.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import Sidebar from "./SideBar";
+
 import Navbar from "./Navbar";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
+import SideBar from "./Sidebar";
 
 export default function MasterLayout({LoggedData}) {
   return (
     <div>
       <div className="d-flex">
         <div className=" ">
-          <Sidebar />
+          <SideBar />
         </div>
         <div className="w-100 bg-warning">
           <Navbar LoggedData={LoggedData} />
